7d7463a3711ca5e2b165724d99144942a201e982
Aruyn <Aruyn@users.noreply.github.com>
2021-03-25 12:17:10 +0100

    StorageContext is now available as a dependecy injection

--- Storage/StorageWebApi/Startup.cs
+++ Storage/StorageWebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using StorageWebApi.Database;
 
 namespace StorageWebApi
 {
@@ -25,6 +26,7 @@ namespace StorageWebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "StorageWebApi", Version = "v1" });
             });
+            services.AddEntityFrameworkSqlite().AddDbContext<StorageContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +49,11 @@ namespace StorageWebApi
             {
                 endpoints.MapControllers();
             });
+
+            using(var client = new StorageContext())
+            {
+                client.Database.EnsureCreated();
+            }
         }
     }
 }
